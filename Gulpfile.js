29b02faa8bfa75fa0005849d2712009b95efba72
Jeff Borders <jeffrey.borders@orbitz.com>
2015-10-27 21:00:43 -0500

    initial commit

---
+++ Gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp'),
+	connect = require('gulp-connect'),
+	sass = require('gulp-sass'),
+	autoPrefixer = require('gulp-autoprefixer'),
+	cssLint = require('gulp-csslint'),
+	minifyCss = require('gulp-minify-css'),
+	browserify = require('gulp-browserify'),
+	jsHint = require('gulp-jshint'),
+	uglify = require('gulp-uglify');
+
+gulp
+	.task('css', function() {
+		gulp
+			.src('styles/**/*.scss')
+			.pipe(sass())
+			.pipe(cssLint())
+			.pipe(autoPrefixer({
+				browsers: ['last 2 versions'],
+				cascade: false
+			}))
+			.pipe(minifyCss({
+				compatibility: 'ie8'
+			}))
+			.pipe(gulp.dest("dist/css"));
+	})
+	.task('js', function() {
+		gulp
+			.src(['lib/**/*.js'])
+			.pipe(jsHint())
+			.pipe(browserify({
+				insertGlobals: true,
+				debug: true
+			}))
+			.pipe(uglify())
+			.pipe(gulp.dest('dist/js'));
+	})
+	.task('connect', function() {
+		connect.server({
+			root: [__dirname],
+			port: 3636,
+			livereload: true,
+			middleware: function(connect, opt) {
+				return [
+					require('./app')
+				]
+			}
+		});
+	});
+
+
+gulp.task('watch', function() {
+	gulp.watch(['styles/**/*.scss'], ['css']);
+	gulp.watch(['lib/**/*.js'], ['js']);
+});
+
+// task runners
+gulp.task('default', ['build', 'connect', 'watch']);
+gulp.task('build', ['css', 'js']);
